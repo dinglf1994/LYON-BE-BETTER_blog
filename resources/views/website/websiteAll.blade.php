db8351eef768c2d5142a4a0aba50045ffa2546eb
dingliangfeng <dingliangfeng@happyjuzi.com>
2017-02-14 14:24:12 +0800

    init

---
+++ resources/views/website/websiteAll.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>网站地址</title>
+    <!-- css -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/website.css">
+    <!-- js -->
+    <script src="/assets/js/jquery-2.1.4.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="web-sty">
+            <a class="label label-primary" href="">TEST-lyon.laravel.cc</a>
+            <a class="label label-primary" href="">admin.juzi.com</a>
+            <a class="label label-primary" target="_blank" href="http://lyon.blog.com/admin/index">lyon.blog.com</a>
+            <a class="label label-primary" href=""></a>
+            <a class="label label-primary" href=""></a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
